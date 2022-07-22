22e69eba2d3353746247c40a197d41014b15dad4
permeoman <108603175+permeoman@users.noreply.github.com>
2022-07-22 23:40:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,36 @@ def generate_password():
 
 window = Tk()
 window.title("Password Generator")
+window.minsize(width=400, height=300)
 
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="img.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=20)
+website_entry.grid(column=1, row=1, padx=5, pady=5)
+email_entry = Entry(width=20)
+email_entry.grid(column=1, row=2, padx=5, pady=5)
+password_entry = Entry(width=20)
+password_entry.grid(column=1, row=3, padx=5, pady=5)
+
+
+# Buttons
+gen_password = Button(text="Generate Password", width=20)
+gen_password.grid(column=2, row=3)
+add_password = Button(text="Add Password", width=20)
+add_password.grid(column=1, row=4)
+search = Button(text="Search", width=20)
+search.grid(column=2, row=1)
 window.mainloop()
 
