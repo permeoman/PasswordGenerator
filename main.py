64e858a2d223b024b7eae1bf8d0c78f435698761
permeoman <108603175+permeoman@users.noreply.github.com>
2022-07-23 00:30:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,28 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(6, 10)
-    nr_numbers = random.randint(4, 6)
-    nr_symbols = random.randint(3, 6)
+    nr_letters = random.randint(7, 10)
+    nr_numbers = random.randint(2, 4)
+    nr_symbols = random.randint(2, 4)
 
-    rand_letters = [random.choice(letters) for char in range(nr_letters)]
-    rand_numbers = [random.choice(numbers) for num in range(nr_numbers)]
-    rand_symbols = [random.choice(symbols) for sym in range(nr_symbols)]
+    rand_letters = [random.choice(letters) for _ in range(nr_letters)]
+    rand_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    rand_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
 
     password_list = rand_letters + rand_numbers + rand_symbols
     random.shuffle(password_list)
 
     password = "".join(password_list)
+    password_entry.insert(0, password)
+
+# ______________________________________Save Password ___________________________________________#
+
 
 # ______________________________________UI SETUP ________________________________________________#
-
 window = Tk()
 window.title("Password Generator")
-window.minsize(width=400, height=300)
+window.minsize(width=500, height=300)
+window.config(padx=50, pady=40)
 
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="img.png")
@@ -44,20 +48,22 @@ password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=20)
+website_entry = Entry(width=30)
 website_entry.grid(column=1, row=1, padx=5, pady=5)
-email_entry = Entry(width=20)
+email_entry = Entry(width=30)
 email_entry.grid(column=1, row=2, padx=5, pady=5)
-password_entry = Entry(width=20)
+password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3, padx=5, pady=5)
 
 
 # Buttons
-gen_password = Button(text="Generate Password", width=20)
+gen_password = Button(text="Generate Password", width=15, command=generate_password)
 gen_password.grid(column=2, row=3)
-add_password = Button(text="Add Password", width=20)
-add_password.grid(column=1, row=4)
-search = Button(text="Search", width=20)
+add_password = Button(text="Add Password", width=16)
+add_password.grid(column=1, row=4, padx=5, pady=5)
+search = Button(text="Search", width=6)
 search.grid(column=2, row=1)
+
+
 window.mainloop()
 
